6acd90d0456a514ca4d1c128ecd5d5129eac63b5
Rafael Magaña <raismaav.2510@gmail.com>
2024-06-16 01:54:34 -0600

    CardProduct.kt added

---
+++ app/src/main/java/com/example/neoarcamovil/CardProduct.kt
@@ -0,0 +1,96 @@
+package com.example.neoarcamovil
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.neoarcamovil.ui.theme.NeoArcaMovilTheme
+
+@Composable
+fun ProductCard(name: String, price: Double) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(Color.Gray)
+        .padding(8.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ProductIcon()
+        ProductAdd()
+        ProductText(name, price)
+    }
+}
+
+@Composable
+fun ProductIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "Product icon",
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun ProductAdd() {
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(32.dp))
+        .background(Color.Blue)
+        .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_remove_24),
+            contentDescription = "Product icon",
+            modifier = Modifier
+        )
+        Text(
+            text = "Agregar al carrito",
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.baseline_add_24),
+            contentDescription = "Product icon",
+            modifier = Modifier
+        )
+    }
+}
+
+@Composable
+fun ProductText(name: String, price: Double) {
+    Text(
+        text = name,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(8.dp)
+    )
+
+    Text(
+        text = "\$$price",
+        fontSize = 16.sp,
+        modifier = Modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductCardPreview() {
+    NeoArcaMovilTheme {
+        ProductCard("Android", 100.0)
+    }
+}
--- app/src/main/java/com/example/neoarcamovil/MainActivity.kt
+++ app/src/main/java/com/example/neoarcamovil/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,29 +18,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NeoArcaMovilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ProductCard("Android", 100.0)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     NeoArcaMovilTheme {
-        Greeting("Android")
+        ProductCard("Android", 100.0)
     }
 }
